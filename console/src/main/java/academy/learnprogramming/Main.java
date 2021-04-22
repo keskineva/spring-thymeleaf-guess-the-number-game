a887640d0c1a40fc07ab682cdcabad2a61a8994b
keskineva <developer@example.com>
2021-04-22 12:57:05 +0300

    updated pom files and added views and message.properties

--- console/src/main/java/academy/learnprogramming/Main.java
+++ console/src/main/java/academy/learnprogramming/Main.java
@@ -1,6 +1,8 @@
 package academy.learnprogramming;
 
 import academy.learnprogramming.config.GameConfig;
+
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
